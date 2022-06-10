5c5d50aedf1d8375d83d7d6ad135ad643afa993f
Nafeeur Rahman <67729867+nafeeur@users.noreply.github.com>
2022-06-10 11:41:40 -0400

    Time: 46 ms (39.91%), Space: 14 MB (12.13%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -1,16 +1,17 @@
-class Solution(object):
-    def reverseOnlyLetters(self, S):
-        ans = []
-        j = len(ans) - 1
-        for i, x in enumerate(S):
-            if x.isalpha():
-                while not S[j].isalpha():
-                    j -= 1
-                ans.append(S[j])
-                j -= 1
-            else:
-                ans.append(x)
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        l,r = 0, len(s)-1
+        s=list(s)
         
-        return "".join(ans)
-            
+        while l<r:
+            if l==r:
+                break
+            while not s[l].isalpha() and l<r:
+                l+=1
+            while not s[r].isalpha() and l<r:
+                r-=1
+            s[l],s[r] = s[r],s[l]
+            l+=1
+            r-=1
+        return "".join(s)
         
