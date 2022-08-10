19983c46e57c6c598f1a8dce8131483d5ac394c3
Nafeeur Rahman <67729867+nafeeur@users.noreply.github.com>
2022-08-10 18:43:53 -0400

    Time: 6069 ms (73.33%), Space: 13.9 MB (89.31%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,38 +1,27 @@
 class Solution:
+          
+    maxGoldFound = 0
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         
-        
-        
-        # row = len(grid), collumn = len(grid[0])
-        # max_output = 0, 2 for loops, dfs helper function
-        # grid[i][j] = 0
-        
-        
-        r = len(grid)
-        c = len(grid[0])
-        max_out = 0
-        def dfs(i,j):
-            if(i >= r or j >= c or i < 0 or j < 0 or grid[i][j] == 0):
-                return 0
-            
-            curr = grid[i][j]
-            grid[i][j] = 0
-            right = dfs(i, j+1)
-            left = dfs(i, j-1)
-            top = dfs(i-1, j)
-            down = dfs(i+1, j)
-            out = max(right,left,top,down) + curr
-            grid[i][j] = curr
-            
-            return out
-        
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j] != 0:
-                    max_out = max(dfs(i,j), max_out)
-        
-      
-        return max_out
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                self.dfs(grid, i, j, 0)
+        return self.maxGoldFound
+
+    def dfs(self, grid, i, j, currGold):
+        width, height = len(grid[0]), len(grid)
+        if i < 0 or j < 0 or i >= height or j >= width or grid[i][j] == 0:
+            return
+        goldInCell = grid[i][j]
+        totalGold = currGold + goldInCell
+        if totalGold > self.maxGoldFound:
+            self.maxGoldFound = totalGold
+        grid[i][j] = 0
+        self.dfs(grid, i - 1, j, totalGold)
+        self.dfs(grid, i + 1, j, totalGold)
+        self.dfs(grid, i, j + 1, totalGold)
+        self.dfs(grid, i, j - 1, totalGold)
+        grid[i][j] = goldInCell
             
     
                     
